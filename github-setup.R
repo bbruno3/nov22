f2002f241d1c84e57ea8b971f6e9eb6d9f4149b9
bbruno3 <bruno3@arizona.edu>
2022-11-22 20:21:05 +0000

    Created R script

---
+++ github-setup.R
@@ -0,0 +1,5 @@
+#Tutorial for linking scripts to Github
+
+#Now we need to configure GitHub in exactly the same way you did previously on the HPC
+
+#In the Terminal Window (not the R Console) type the following commands
